61955498b442701105eb1318b2ca87984679bbac
Kirill Zaborsky <avto.oil@gmail.com>
2021-09-13 15:21:31 +0700

    Initial Commit

---
+++ Guees it/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guees it
+//
+//  Created by Кирилл Заборский on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
